0bb174958b33c1b1066c0d414c3185ad05828281
Abhishekkulkarni1997 <abhishekkulkarni1997@gmail.com>
2023-03-18 23:37:19 +0530

    added provider & components to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import "bootstrap/dist/css/bootstrap.css.min";
 import "./App.css";
+import { Container } from "reactstrap";
+import Todo from "./Components/Todo";
+import TodoForm from "./Components/TodoForm";
+import { Provider } from "react-redux";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <Provider>
+      <Container>
+        <Todo />
+        <TodoForm />
+      </Container>
+    </Provider>
+  );
 }
 
 export default App;
